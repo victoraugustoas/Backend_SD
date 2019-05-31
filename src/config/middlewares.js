d2ccfd61edd308d3a3058a478216ffc658e21a22
Victor Augusto Andrade Silva <vaas@icomp.ufam.edu.br>
2019-05-31 11:06:59 -0400

    Adicionando cors para uso externo

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,6 +1,10 @@
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 module.exports = (app) => {
+    // habilita para uso externo
+    app.use(cors())
+
     // faz o parser dos objetos json
     app.use(bodyParser.json())
 }
