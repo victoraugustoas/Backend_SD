3ad19c022d8dc063a7868582f539b6a6ec242e70
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-06-05 21:25:55 -0400

    Documentada a rota de cadastro de usuário

--- src/config/routes/user.js
+++ src/config/routes/user.js
@@ -3,6 +3,45 @@ const router = express.Router()
 
 module.exports = (app) => {
 
+    /**
+     * @apiName User
+     */
+
+    /**
+     * 
+     * @api {post} /user/ Cadastro de usuário
+     * @apiGroup User
+     * 
+     * 
+     * @apiParam (BodyParams) {String} name Nome do usuário
+     * @apiParam (BodyParams) {String} email Email do usuário
+     * @apiParam (BodyParams) {Boolean} gender Sexo do usuário
+     * @apiParam (BodyParams) {Boolean} gender Sexo do usuário
+     * @apiParam (BodyParams) {String} password Senha do usuário
+     * @apiParam (BodyParams) {String} dateOfBirth Data de aniversário do usuário
+     * @apiParam (BodyParams) {String} [urlImg] Imagem do usuário
+     * @apiParam (BodyParams) {String} isPremium=false Imagem do usuário
+     * 
+     * @apiSuccess (Sucess 201) {json} msg Mensagem de criado com sucesso
+     * 
+     * @apiParamExample {json} {BodyParams} Request-Example:
+    * {
+    "name": "Victor Augusto",
+    "email": "victoraugusto@hotmail.com",
+    "gender": "false",
+    "password": "1234",
+    "dateOfBirth": "14/03/1998",
+    "isPremium": "true"
+}
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "msg": "Usuário cadastrado com sucesso!"
+     * }
+     * 
+     * 
+     */
     router.post('/', app.user.save)
     router.get('/:id', app.user.getByID)
     router.put('/:id', app.user.edit)
