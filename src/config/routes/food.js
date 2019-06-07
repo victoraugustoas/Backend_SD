23f24b5b190ca2b518a749690b9ff0b3696e5c1d
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-06-06 21:17:53 -0400

    Documentação

--- src/config/routes/food.js
+++ src/config/routes/food.js
@@ -3,14 +3,279 @@ const router = express.Router()
 
 module.exports = (app) => {
 
+
     // CRUD
     router.post('/', app.auth.authenticate(), app.food.save)
+    /**
+     * 
+     * @api {get} /food/:id Informações do alimento e similares
+     * @apiGroup Food
+     * 
+     * @apiHeader  (HeaderParams) {Token} Authorization Token de autenticação
+     * @apiParam (URLParams) {ObjectID} ID ID do alimento
+     * 
+     * @apiSuccess (Sucess 200) {Object} food Alimento com os nutrientes, usuários premium e free
+     * @apiSuccess (Sucess 200) {Object[]} lstSimilars Array com alimentos similares ao atributo food, somente para usuários premium
+     * 
+     * @apiParamExample  {URL Params} Request-Example:
+     * /food/5cf845fe245413a64b405024
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response (Usuário Free):
+     * {
+    "name": {
+        "value": "queijo requeijão cremoso"
+    },
+    "category": {
+        "value": "leite e derivados"
+    },
+    "energy": {
+        "value": 1382.239,
+        "type": "Kj"
+    },
+    "humidity": {
+        "value": 62.5,
+        "type": "%"
+    },
+    "protein": {
+        "value": 12.355,
+        "type": "g"
+    },
+    "lipids": {
+        "value": 30.116,
+        "type": "g"
+    },
+    "carbohydrate": {
+        "value": 3.089,
+        "type": "g"
+    }
+}
+     * 
+     * @apiSuccessExample {json} Success-Response (Usuário Premium):
+     * {
+    "food": {
+        "humidity": {
+            "value": 62.5,
+            "type": "%"
+        },
+        "energy": {
+            "value": 1382.239,
+            "type": "Kj"
+        },
+        "protein": {
+            "value": 12.355,
+            "type": "g"
+        },
+        "lipids": {
+            "value": 30.116,
+            "type": "g"
+        },
+        "cholesterol": {
+            "value": 95.238,
+            "type": "mg"
+        },
+        "carbohydrate": {
+            "value": 3.089,
+            "type": "g"
+        },
+        "food fiber": {
+            "value": "Null",
+            "type": "g"
+        },
+        "ashes": {
+            "value": 2.574,
+            "type": "g"
+        },
+        "calcium": {
+            "value": 333.333,
+            "type": "mg"
+        },
+        "magnesium": {
+            "value": 15.444,
+            "type": "mg"
+        },
+        "manganese": {
+            "value": 0.026,
+            "type": "mg"
+        },
+        "phosphorus": {
+            "value": 576.577,
+            "type": "mg"
+        },
+        "iron": {
+            "value": 0.129,
+            "type": "mg"
+        },
+        "sodium": {
+            "value": 718.147,
+            "type": "mg"
+        },
+        "potassium": {
+            "value": 119.691,
+            "type": "mg"
+        },
+        "copper": {
+            "value": 0.064,
+            "type": "mg"
+        },
+        "zinc": {
+            "value": 1.673,
+            "type": "mg"
+        },
+        "retinol": {
+            "value": 250.965,
+            "type": "mcg"
+        },
+        "re": {
+            "value": "Null",
+            "type": "mcg"
+        },
+        "rae": {
+            "value": "Null",
+            "type": "mcg"
+        },
+        "thiamine": {
+            "value": "Null",
+            "type": "mg"
+        },
+        "riboflavin": {
+            "value": 0.245,
+            "type": "mg"
+        },
+        "pyridoxine": {
+            "value": "Null",
+            "type": "mg"
+        },
+        "niacin": {
+            "value": "Null",
+            "type": "mg"
+        },
+        "vitamin c": {
+            "value": "Null",
+            "type": "mg"
+        },
+        "_id": "5cf845fe245413a64b405024",
+        "name": {
+            "value": "queijo requeijão cremoso"
+        },
+        "category": {
+            "value": "leite e derivados"
+        }
+    },
+    "lstSimilars": [...]
+}
+     */
     router.get('/:id', app.auth.authenticate(), app.food.getByID)
     router.put('/:id', app.auth.authenticate(), app.food.edit)
     router.delete('/:id', app.auth.authenticate(), app.food.erase)
 
     // Buscas
+    /**
+     * 
+     * @api {get} /food/search/nutrient/ Ranking de alimentos para determinado nutriente
+     * @apiGroup Food
+     * @apiHeader (HeaderParams) {Token} Authorization Token de autenticação
+     * 
+     * 
+     * @apiParam (QueryStringParams) {String} nutrient Nome do nutriente
+     * @apiParam (QueryStringParams) {String} sort Podendo variar entre (asc, desc)
+     * @apiParam (QueryStringParams) {Int} limit Limite de documentos a ser retornado
+     * @apiParam (QueryStringParams) {Int} page Número da página a ser retornada na paginação
+     * 
+     * @apiSuccess (Sucess 200) {Int} page Diz a página atual retornada pela paginação
+     * @apiSuccess (Sucess 200) {Int} limit Limite de documentos presentes em data
+     * @apiSuccess (Sucess 200) {Object[]} data Array de alimentos
+     * 
+     * @apiParamExample  {Query String} Request-Example:
+     * /food/search/nutrient/?nutrient=lipids&sort=desc&limit=5&page=2
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+    "page": 2,
+    "limit": 2,
+    "data": [...]
+}
+     */
     router.get('/search/nutrient/', app.auth.authenticate(), app.food.searchByNutrient)
+    /**
+     * 
+     * @api {post} /food/search/name/ Busca por nome de alimento
+     * @apiDescription Array de alimentos que casam com a string
+     * @apiGroup Food
+     * 
+     * @apiHeader  (HeaderParams) {Token} Authorization Token de autenticação
+     * @apiParam (BodyParams) {String} name Nome do alimento a ser buscado
+     * 
+     * 
+     * @apiParamExample  {Body Params} Request-Example:
+     * {
+     *     "name" : "arroz"
+     * }
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * [
+    {
+        "name": {
+            "value": "arroz integral cozido"
+        },
+        "category": {
+            "value": "cereais e derivados"
+        },
+        "energy": {
+            "value": 786.91,
+            "type": "Kj"
+        },
+        "humidity": {
+            "value": 70.1,
+            "type": "%"
+        },
+        "protein": {
+            "value": 3.957,
+            "type": "g"
+        },
+        "lipids": {
+            "value": 1.522,
+            "type": "g"
+        },
+        "carbohydrate": {
+            "value": 39.269,
+            "type": "g"
+        }
+    },
+    {
+        "name": {
+            "value": "noz crua"
+        },
+        "category": {
+            "value": "leguminosas e derivados"
+        },
+        "energy": {
+            "value": 1267.84,
+            "type": "Kj"
+        },
+        "humidity": {
+            "value": 6.2,
+            "type": "%"
+        },
+        "protein": {
+            "value": 6.843,
+            "type": "g"
+        },
+        "lipids": {
+            "value": 29.032,
+            "type": "g"
+        },
+        "carbohydrate": {
+            "value": 8.993,
+            "type": "g"
+        }
+    }
+]
+     * 
+     * 
+     */
     router.post('/search/name/', app.auth.authenticate(), app.food.searchByName)
 
     app.use('/food', router)
--- src/config/routes/user.js
+++ src/config/routes/user.js
@@ -3,11 +3,173 @@ const router = express.Router()
 
 module.exports = (app) => {
 
+    /**
+     * @apiName User
+     */
+
+    /**
+     * 
+     * @api {post} /user/ Cadastro de usuário
+     * @apiGroup User
+     * 
+     * 
+     * @apiParam (BodyParams) {String} name Nome do usuário
+     * @apiParam (BodyParams) {String} email Email do usuário
+     * @apiParam (BodyParams) {Boolean} gender Sexo do usuário
+     * @apiParam (BodyParams) {Boolean} gender Sexo do usuário
+     * @apiParam (BodyParams) {String} password Senha do usuário
+     * @apiParam (BodyParams) {String} dateOfBirth Data de aniversário do usuário
+     * @apiParam (BodyParams) {String} [urlImg] Imagem do usuário
+     * @apiParam (BodyParams) {String} isPremium=false Imagem do usuário
+     * 
+     * @apiSuccess (Sucess 201) {json} msg Mensagem de criado com sucesso
+     * 
+     * @apiParamExample {json} {BodyParams} Request-Example:
+    * {
+    "name": "Victor Augusto",
+    "email": "victoraugusto@hotmail.com",
+    "gender": "false",
+    "password": "1234",
+    "dateOfBirth": "14/03/1998",
+    "isPremium": "true"
+}
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "msg": "Usuário cadastrado com sucesso!"
+     * }
+     * 
+     * 
+     */
     router.post('/', app.user.save)
+    /**
+     * 
+     * @api {get} /user/:id Recupera dados do usuário
+     * @apiGroup User
+     * 
+     * @apiHeader  (HeaderParams) {Token} Authorization Token de autenticação
+     * @apiParam (URLParams) {ObjectID} ID ID do usuário
+     * 
+     * @apiSuccess (Sucess 200) {json} data Dados do usuário
+     * 
+     * @apiParamExample  {type} Request-Example:
+     * /user/5cf83fa7fe5e9e281e21b7b2
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+    "urlImg": "",
+    "_id": "5cf83fa7fe5e9e281e21b7b2",
+    "name": "Victor Augusto2",
+    "email": "victoraugustoandradesilva2@hotmail.com",
+    "gender": false,
+    "dateOfBirth": "14/03/1998"
+}
+     * 
+     * 
+     */
     router.get('/:id', app.user.getByID)
+    /**
+     * 
+     * @api {put} /user/:id Altera os dados do usuário
+     * @apiGroup User
+     * 
+     * @apiHeader  (HeaderParams) {Token} Authorization Token de autenticação
+     * @apiParam (URLParams) {ObjectID} ID ID do usuário que será alterado
+     * @apiParam (BodyParams) {String} name Nome do usuário
+     * @apiParam (BodyParams) {String} email Email do usuário
+     * @apiParam (BodyParams) {Boolean} gender Sexo do usuário
+     * @apiParam (BodyParams) {Boolean} gender Sexo do usuário
+     * @apiParam (BodyParams) {String} password Senha do usuário
+     * @apiParam (BodyParams) {String} dateOfBirth Data de aniversário do usuário
+     * @apiParam (BodyParams) {String} [urlImg] Imagem do usuário
+     * @apiParam (BodyParams) {String} isPremium=false Imagem do usuário
+     *
+     * 
+     * 
+     * @apiSuccess (Sucess 200) {json} msg Mensagem de confirmação de alteração
+     * 
+     * @apiParamExample  {json} {BodyParams} Request-Example:
+     * {
+    "urlImg": "",
+    "name": "Victor Augusto",
+    "email": "victoraugustoandradesilva@hotmail.com",
+}
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "msg" : "Usuário alterado com sucesso."
+     * }
+     * 
+     * 
+     */
     router.put('/:id', app.user.edit)
+    /**
+     * 
+     * @api {delete} /user/:id Apaga o usuário do sistema
+     * @apiGroup User
+     * 
+     * @apiHeader  (HeaderParams) {Token} Authorization Token de autenticação
+     * @apiParam (URLParams) {ObjectID} ID ID do usuário
+     * 
+     * @apiSuccess (Sucess 200) {json} msg Mensagem de sucesso
+     * 
+     * @apiParamExample  {URLParams} Request-Example:
+     * /user/5cf83fa7fe5e9e281e21b7b2
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "msg" : "Usuário deletado com sucesso."
+     * }
+     * 
+     * 
+     */
     router.delete('/:id', app.user.erase)
 
+    /**
+     * 
+     * @api {post} /user/auth Autentica o usuário no sistema
+     * @apiGroup Auth
+     * 
+     * 
+     * @apiParam (BodyParams) {String} email Email do usuário
+     * @apiParam (BodyParams) {String} password Senha do usuário
+     * 
+     * @apiSuccess (Sucess 200) {ObjectID} _id ID do usuário
+     * @apiSuccess (Sucess 200) {String} name Nome do usuário
+     * @apiSuccess (Sucess 200) {String} email Email do usuário
+     * @apiSuccess (Sucess 200) {Boolean} isPremium Informa se o usuário é premium
+     * @apiSuccess (Sucess 200) {String} urlImg Url da imagem do usuário
+     * @apiSuccess (Sucess 200) {Boolean} gender Sexo do usuário
+     * @apiSuccess (Sucess 200) {Int} iat Token emitido na data iat, definido em segundos
+     * @apiSuccess (Sucess 200) {Int} exp Data de expiração do token em segundos
+     * @apiSuccess (Sucess 200) {String} token Token de autenticação
+     * 
+     * @apiParamExample  {json} {BodyParams} Request-Example:
+     * {
+     *     "email" : "alguem@email.com",
+     *     "password": "123"
+     * }
+     * 
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * {
+    "_id": "5cf72b58b83ba43f21078914",
+    "name": "Victor Augusto",
+    "email": "alguem@email.com",
+    "isPremium": false,
+    "urlImg": "",
+    "gender": false,
+    "iat": 1559703596,
+    "exp": 1559962796,
+    "token": "eyJ0eXAiOiJKV1QiLCJhbxciOiJIUzI1NiJ9.eyJfaWQiOiI1Y2Y3MmI1OGI4M2JhNzNmMjEwNzg5MTQiLCJuYW1lIjoiVmljdG9yIEF1Z3VzdG8iLCJlbWFpbCI6InZpY3RvcmF1Z3VzdG9hbmRyYWRlc2lsdmFAaG90bWFpbC5jb20iLCJpc1ByZW1pdW0iOmZhbHNlLCJ1cmxJbWciOiIiLCJnZW5kZXIiOmZhbHNlLCJpYXQiOjE1NTk3MDM1OTYsImV4cCI6MTU1OTk2Mjc5Nn0.HOs5ZSV6ILimD3lGUv7wLkFHYPjyNWrnIzsW2vTjES8"
+}
+     * 
+     * 
+     */
     router.post('/auth', app.auth.signin)
     router.post('/auth/validateToken', app.auth.validateToken)
 
