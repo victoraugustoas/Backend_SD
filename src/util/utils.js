f7489e77080ccbc8d74e82797264fc21d54a4f26
Victor Augusto <38368198+victoraugustoas@users.noreply.github.com>
2019-07-09 11:03:52 -0400

    Update utils.js

--- src/util/utils.js
+++ src/util/utils.js
@@ -18,7 +18,7 @@ module.exports = {
                 status
             }
         }
-        if (field) {
+        if (field != []) {
             throw {
                 msg,
                 status
